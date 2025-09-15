c2eea951cbbbc3d4866a7706cb0d8712cb1a21e7
Hyden Liu <sunupdong@gmail.com>
2025-09-15 16:57:01 +0800

    chore: format

---
+++ src/lib/download.ts
@@ -0,0 +1,24 @@
+import { saveAs } from 'file-saver'
+import JsZip from 'jszip'
+
+export type LangMap = Record<string, any>
+
+/**
+ * 导出多个文件到zip压缩包
+ */
+export async function downloadFilesToZip(params: LangMap) {
+	const zip = new JsZip()
+	const promises = Object.keys(params).map((key: string) =>
+		downLoadJSON(params[key], zip, key),
+	)
+	await Promise.all(promises)
+	zip.generateAsync({ type: 'blob' }).then((blob) => {
+		saveAs(blob, `excel-i18n-${Date.now()}.zip`)
+	})
+}
+
+export function downLoadJSON(data: LangMap, zip: JsZip, fileName: string) {
+	const jsr = JSON.stringify(data, null, 2)
+	const blob = new Blob([jsr], { type: 'application/json' })
+	zip.file(`${fileName}.json`, blob)
+}
